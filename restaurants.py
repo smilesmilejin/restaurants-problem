e5062a5946051673c21ce49d06dfa7f9575f6e80
Xinshuang Jin <jinxinshuang@gmail.com>
2025-06-05 14:25:07 -0400

    changes on 2025/06/05

--- restaurants.py
+++ restaurants.py
@@ -1,6 +1,32 @@
 def most_varied_visitor(visits):
     # Write your solution here!
-    pass
+    # pass
+
+    # create dictionary callled persons, persons:count
+    # loop through the persons list in the visits
+        # if it is not in the dictionary, initialize it
+        # if it is in the dictionary, add 1 to the count
+
+    # loop through the persons, find the person with max count
+    
+    person_counts = {}
+    for persons in visits.values():
+        for person in set(persons):
+            if person in person_counts:
+                person_counts[person] += 1
+            else:
+                person_counts[person] = 1
+    
+    # print(person_counts)
+
+    most_person = None
+    max_count = 0
+    for person, count in person_counts.items():
+        if count > max_count:
+            most_person = person
+            max_count = count
+    
+    return most_person
 
 
 visits_1 = {
